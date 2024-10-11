43bc46550f564e66d4a90a40399ff304717822c0
zencak@ttc.cz <zencak@ttc.cz>
2024-10-11 22:24:12 +0200

    fixed footer not sticking to page bottom

--- frontend/src/modules/auth/templates/LogInTemplate.tsx
+++ frontend/src/modules/auth/templates/LogInTemplate.tsx
@@ -1,4 +1,4 @@
-import { Center, Link, Paragraph } from '@frontend/shared/design-system';
+import { Box, Center, Link, Paragraph } from '@frontend/shared/design-system';
 import Footer from '@frontend/shared/navigation/components/footer/Footer';
 import Navbar from '@frontend/shared/navigation/components/navbar/Navbar';
 
@@ -9,32 +9,48 @@ export type LogInTemplateProps = {
   error?: Error;
   onSubmit: (data: { email: string; password: string }) => void;
 };
-
+// todo - on extreme vertical shrink login window 'hides' behind navbar - fix pls
 export function LogInTemplate({
   isLoading,
   error,
   onSubmit,
 }: LogInTemplateProps) {
   return (
-    <>
+    <Box display="flex" flexDirection="column" minHeight="100vh">
       <Navbar />
-      <LogInForm
-        isLoading={isLoading}
-        errorMessage={error && error.message}
-        onSubmit={onSubmit}
+      <Box
+        flex="1"
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+        overflowY="auto"
+        padding={{ base: '4', sm: '6', md: '8' }}
+        maxWidth={{ base: '100%', sm: '80%', md: '60%' }}
+        mx="auto"
       >
-        <Center>
-          <Link fontSize={'l'} href="/">
-            Forgotten password?
-          </Link>
-        </Center>
-        <Center>
-          <Paragraph>
-            Don't have account? <Link href="/auth/signup">Sign up</Link>
-          </Paragraph>
-        </Center>
-      </LogInForm>
+        <LogInForm
+          isLoading={isLoading}
+          errorMessage={error && error.message}
+          onSubmit={onSubmit}
+        >
+          <Center>
+            <Link
+              fontSize={{ base: 'xs', sm: 'sm' }} // todo - tweak those so it looks good
+              href="/"
+            >
+              Forgotten password?
+            </Link>
+          </Center>
+          <Center>
+            <Paragraph
+              fontSize={{ base: 'xs', sm: 'md' }} // tweak those too
+            >
+              Don't have an account? <Link href="/auth/signup">Sign up</Link>
+            </Paragraph>
+          </Center>
+        </LogInForm>
+      </Box>
       <Footer />
-    </>
+    </Box>
   );
 }
