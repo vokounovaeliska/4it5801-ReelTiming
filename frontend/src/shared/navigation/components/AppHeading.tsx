e8866c24ee648366c1bb7846e44797b7acf4cd1e
Matěj Ženčák <zenm04@vse.cz>
2024-10-11 12:21:45 +0000

    Resolve "Základní layout aplikace"

---
+++ frontend/src/shared/navigation/components/AppHeading.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Heading } from '@chakra-ui/react';
+
+const AppHeading: React.FC = () => {
+  return (
+    <Heading as="h1" size="lg" color="white" mr={4}>
+      ReelTiming
+    </Heading>
+  );
+};
+
+export default AppHeading;
