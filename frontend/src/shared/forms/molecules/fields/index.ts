a4e4fd6e8b58a0d736f5c5eccc3afebdb0c39c01
zencak@ttc.cz <zencak@ttc.cz>
2024-10-15 15:53:04 +0200

    conflicts resolve

--- frontend/src/shared/forms/molecules/fields/index.ts
+++ frontend/src/shared/forms/molecules/fields/index.ts
@@ -1,9 +1,6 @@
 export { CheckboxField, type CheckboxFieldProps } from './CheckboxField';
 export { InputField, type InputFieldProps } from './InputField';
-<<<<<<< HEAD
 export {
   SingleFileUploadField,
   type SingleFileUploadFieldProps,
 } from './SingleFileUploadField';
-=======
->>>>>>> c6d6e61 (fixed prettier/lint warnings/errors)
