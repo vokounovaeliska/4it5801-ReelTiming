8506888ce243dbbea2514605454e643187715668
vzorny_student <capj05@vse.cz>
2024-10-16 17:30:34 +0200

    projectResolver - add addProject mutation

--- backend/src/graphql/modules/project/projectResolver.ts
+++ backend/src/graphql/modules/project/projectResolver.ts
@@ -6,7 +6,7 @@ import { project } from '@backend/db/schema';
 import { CustomContext } from '../../../types/types';
 
 import { Project } from './projectType';
-//import { GraphQLError } from 'graphql/error';
+import { GraphQLError } from 'graphql/error';
 
 @Resolver(() => Project)
 export class ProjectResolver {
@@ -26,51 +26,51 @@ export class ProjectResolver {
     }));
   }
 
-  //@Mutation(() => Project)
-  //async addProject(
-  //  @Arg('name') name: string,
-  //  @Arg('production_company', { nullable: true }) productionCompany: string | null,
-  //  @Arg('start_date', { nullable: true }) startDate: Date | null,
-  //  @Arg('end_date', { nullable: true }) endDate: Date | null,
-  //  @Ctx() { db }: CustomContext
-  //): Promise<Project> {
-  //  const createdAt = new Date();
-  //
-  //  const newProject = await db
-  //    .insert(project)
-  //    .values({
-  //      name,
-  //      production_company: productionCompany,
-  //      start_date: startDate,
-  //      end_date: endDate,
-  //      create_date: createdAt,
-  //      create_user_id: 'user-id', // Replace with actual user ID
-  //      last_update_user_id: 'user-id', // Replace with actual user ID
-  //      last_update_date: createdAt,
-  //      is_active: true,
-  //    })
-  //    .$returningId(); // Get the inserted row
-  //
-  //  const id = newProject[0].id;
-  //  const result = await db
-  //    .select()
-  //    .from(project)
-  //    .where(eq(project.id, id));
-  //
-  //  if (result.length === 0) {
-  //    throw new GraphQLError('Project not found');
-  //  }
-  //
-  //  const proj = result[0];
-  //
-  //  return {
-  //    ...proj,
-  //    create_date: new Date(proj.create_date),
-  //    start_date: proj.start_date ? new Date(proj.start_date) : null,
-  //    end_date: proj.end_date ? new Date(proj.end_date) : null,
-  //    is_active: !!proj.is_active,
-  //  } as Project;
-  //}
+  @Mutation(() => Project)
+  async addProject(
+    @Arg('name') name: string,
+    @Arg('production_company') productionCompany: string,
+    @Arg('start_date', { nullable: true }) startDate: Date,
+    @Arg('end_date', { nullable: true }) endDate: Date,
+    @Ctx() { db }: CustomContext
+  ): Promise<Project> {
+    const createdAt: Date = new Date();
+
+    const newProject = await db
+      .insert(project)
+      .values({
+        name,
+        production_company: productionCompany,
+        start_date: startDate,
+        end_date: endDate,
+        create_date: createdAt,
+        create_user_id: 'user-id', // Replace with actual user ID
+        last_update_user_id: 'user-id', // Replace with actual user ID
+        last_update_date: createdAt,
+        is_active: true,
+      })
+      .$returningId();
+
+    const id = newProject[0].id;
+    const result = await db
+      .select()
+      .from(project)
+      .where(eq(project.id, id));
+
+    if (result.length === 0) {
+      throw new GraphQLError('Project not found');
+    }
+
+    const proj = result[0];
+
+    return {
+      ...proj,
+      create_date: new Date(proj.create_date),
+      start_date: proj.start_date ? new Date(proj.start_date) : null,
+      end_date: proj.end_date ? new Date(proj.end_date) : null,
+      is_active: !!proj.is_active,
+    } as Project;
+  }
 
   @Mutation(() => Boolean)
   async deleteProject(
