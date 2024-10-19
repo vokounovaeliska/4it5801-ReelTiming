75c86d06a4dc12ce19bdc05887b24a5cd21f55ba
zencak@ttc.cz <zencak@ttc.cz>
2024-10-19 04:20:20 +0200

    add navbar

--- frontend/src/modules/myprojects/pages/ProjectsDetailPage.tsx
+++ frontend/src/modules/myprojects/pages/ProjectsDetailPage.tsx
@@ -3,6 +3,9 @@ import { gql, useQuery } from '@apollo/client';
 import { Box, Heading, Spinner, Text } from '@chakra-ui/react';
 import { useParams } from 'react-router-dom';
 
+import Navbar from '@frontend/shared/navigation/components/navbar/Navbar';
+import UserNavbar from '@frontend/shared/navigation/components/navbar/UserNavbar';
+
 const GET_PROJECT_DETAIL = gql`
   query GetProjectDetail($id: String!) {
     project(id: $id) {
@@ -39,43 +42,51 @@ export function MyProjectDetailPage() {
   const project = data?.project;
 
   return (
-    <Box p={6}>
-      <Heading as="h2" size="xl" mb={4}>
-        {project?.name}
-      </Heading>
-      <Text fontSize="lg" mb={6}>
-        {project?.description || 'No description available'}
-      </Text>
-      <Text fontSize="md" mb={2}>
-        <strong>Production Company:</strong> {project?.production_company}
-      </Text>
-      <Text fontSize="md" mb={2}>
-        <strong>Start Date:</strong>{' '}
-        {new Date(project?.start_date).toLocaleDateString() || 'N/A'}
-      </Text>
-      <Text fontSize="md" mb={2}>
-        <strong>End Date:</strong>{' '}
-        {project?.end_date
-          ? new Date(project?.end_date).toLocaleDateString()
-          : 'N/A'}
-      </Text>
-      <Text fontSize="md" mb={2}>
-        <strong>Created On:</strong>{' '}
-        {new Date(project?.create_date).toLocaleDateString()}
-      </Text>
-      <Text fontSize="md" mb={2}>
-        <strong>Created By:</strong> {project?.create_user_id}
-      </Text>
-      <Text fontSize="md" mb={2}>
-        <strong>Last Updated On:</strong>{' '}
-        {new Date(project?.last_update_date).toLocaleDateString()}
-      </Text>
-      <Text fontSize="md" mb={2}>
-        <strong>Last Updated By:</strong> {project?.last_update_user_id}
-      </Text>
-      <Text fontSize="md" mb={6}>
-        <strong>Is Active:</strong> {project?.is_active ? 'Yes' : 'No'}
-      </Text>
+    <Box
+      display="flex"
+      flexDirection="column"
+      minHeight="100vh"
+      bgColor="gray.50"
+    >
+      <Navbar children1={<UserNavbar />} />
+      <Box flex="1" p={6}>
+        <Heading as="h2" size="xl" mb={4}>
+          {project?.name}
+        </Heading>
+        <Text fontSize="lg" mb={6}>
+          {project?.description || 'No description available'}
+        </Text>
+        <Text fontSize="md" mb={2}>
+          <strong>Production Company:</strong> {project?.production_company}
+        </Text>
+        <Text fontSize="md" mb={2}>
+          <strong>Start Date:</strong>{' '}
+          {new Date(project?.start_date).toLocaleDateString() || 'N/A'}
+        </Text>
+        <Text fontSize="md" mb={2}>
+          <strong>End Date:</strong>{' '}
+          {project?.end_date
+            ? new Date(project?.end_date).toLocaleDateString()
+            : 'N/A'}
+        </Text>
+        <Text fontSize="md" mb={2}>
+          <strong>Created On:</strong>{' '}
+          {new Date(project?.create_date).toLocaleDateString()}
+        </Text>
+        <Text fontSize="md" mb={2}>
+          <strong>Created By:</strong> {project?.create_user_id}
+        </Text>
+        <Text fontSize="md" mb={2}>
+          <strong>Last Updated On:</strong>{' '}
+          {new Date(project?.last_update_date).toLocaleDateString()}
+        </Text>
+        <Text fontSize="md" mb={2}>
+          <strong>Last Updated By:</strong> {project?.last_update_user_id}
+        </Text>
+        <Text fontSize="md" mb={6}>
+          <strong>Is Active:</strong> {project?.is_active ? 'Yes' : 'No'}
+        </Text>
+      </Box>
     </Box>
   );
 }
