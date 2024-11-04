d7cf26dd1ffd878f25fcadc66dc0ff5d0254e041
Martin Spacek <spam13@vse.cz>
2024-11-04 20:25:53 +0000

    Resolve "Dashboard aktualizace informaci"

---
+++ frontend/src/modules/myprojects/CrewMemberCount.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { useQuery } from '@apollo/client';
+import { Alert, AlertIcon, Spinner } from '@chakra-ui/react';
+
+import { GET_CREWLIST_INFO } from '../../gql/queries/GetCrewListInfo';
+
+interface CrewMemberCountProps {
+  projectId: string;
+  userId: string;
+}
+
+const CrewMemberCount: React.FC<CrewMemberCountProps> = ({
+  projectId,
+  userId,
+}) => {
+  const { loading, error, data } = useQuery(GET_CREWLIST_INFO, {
+    variables: { projectId, userId },
+  });
+
+  if (loading) return <Spinner size="lg" />;
+
+  if (error) {
+    return (
+      <Alert status="error">
+        <AlertIcon />
+        Error loading crew information.
+      </Alert>
+    );
+  }
+
+  const crewMemberCount = data?.projectUsers?.length || 0;
+
+  return <>{crewMemberCount}</>;
+};
+
+export default CrewMemberCount;
