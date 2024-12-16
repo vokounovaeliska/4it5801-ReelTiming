42d5588e6b74d64af3f713e45f20bae85b323e0f
zencak@ttc.cz <zencak@ttc.cz>
2024-12-16 14:26:19 +0100

    add labels to time from/to filter users

--- frontend/src/modules/timesheets/timesheetFilter.tsx
+++ frontend/src/modules/timesheets/timesheetFilter.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import Select, { ActionMeta, MultiValue } from 'react-select';
 
-import { Box, Input } from '@frontend/shared/design-system';
+import {
+  Box,
+  FormControl,
+  FormLabel,
+  Input,
+} from '@frontend/shared/design-system';
 
 import { UserOption } from './interfaces';
 
@@ -37,37 +42,52 @@ export function TimesheetFilter({
       pb={{ base: '4', sm: '2' }}
       pt={{ base: '0', sm: '4' }}
     >
-      <Input
-        type="date"
-        placeholder="Start Date"
-        value={startDate}
-        onChange={(e) => handleDateChange(e, 'start')}
-        width={{ base: '100%', sm: '250px', md: '300px' }}
-      />
-      <Input
-        borderWidth={2}
-        borderColor={'gray.200'}
-        type="date"
-        placeholder="End Date"
-        value={endDate}
-        onChange={(e) => handleDateChange(e, 'end')}
-        width={{ base: '100%', sm: '250px', md: '300px' }}
-      />
+      <Box width={{ base: '100%', sm: 'auto' }}>
+        <FormControl>
+          <FormLabel>Date from</FormLabel>
+          <Input
+            type="date"
+            placeholder="Start Date"
+            value={startDate}
+            onChange={(e) => handleDateChange(e, 'start')}
+            width={{ base: '100%', sm: '250px', md: '300px' }}
+          />
+        </FormControl>
+      </Box>
+      <Box width={{ base: '100%', sm: 'auto' }}>
+        <FormControl>
+          <FormLabel>Date to</FormLabel>
+          <Input
+            borderWidth={2}
+            borderColor={'gray.200'}
+            type="date"
+            placeholder="End Date"
+            value={endDate}
+            onChange={(e) => handleDateChange(e, 'end')}
+            width={{ base: '100%', sm: '250px', md: '300px' }}
+          />
+        </FormControl>
+      </Box>
       {userRole === 'ADMIN' && (
-        <Select
-          isMulti
-          options={userOptions}
-          placeholder="Select Users"
-          onChange={handleUserChange}
-          styles={{
-            container: (provided) => ({
-              ...provided,
-              width: '100%',
-              maxWidth: '350px',
-              zIndex: 100,
-            }),
-          }}
-        />
+        <Box width={{ base: '100%', sm: 'auto' }}>
+          <FormControl>
+            <FormLabel>Filter users</FormLabel>
+            <Select
+              isMulti
+              options={userOptions}
+              placeholder="Select Users"
+              onChange={handleUserChange}
+              styles={{
+                container: (provided) => ({
+                  ...provided,
+                  width: '100%',
+                  maxWidth: '350px',
+                  zIndex: 100,
+                }),
+              }}
+            />
+          </FormControl>
+        </Box>
       )}
     </Box>
   );
