ad024448e51cb5342d5df901f0b6df3e977326ab
jdolecek <jandolecek@outlook.com>
2024-12-25 20:46:07 +0100

    adding tooltips to rate, project and car sections in crewList form

--- frontend/src/modules/crewlist/atoms/ProjectInformationSection.tsx
+++ frontend/src/modules/crewlist/atoms/ProjectInformationSection.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import {
+  Box,
   FormControl,
   FormErrorMessage,
   FormLabel,
   Select,
   SimpleGrid,
+  Tooltip,
 } from '@chakra-ui/react';
 import { Controller } from 'react-hook-form';
 
@@ -25,50 +27,81 @@ export const ProjectInformationSection: React.FC<
     fontSize="1.7rem"
   >
     <SimpleGrid columns={{ base: 1, sm: 2, md: 3 }} spacing={4}>
-      <Controller
-        name="department"
-        render={({ field, fieldState }) => (
-          <FormControl
-            isRequired={userRole === 'ADMIN'}
-            isInvalid={!!fieldState.error}
-          >
-            <FormLabel>Department</FormLabel>
-            <Select
-              {...field}
-              placeholder="Select Department"
-              borderColor="gray.400"
-              borderWidth={1}
-              isDisabled={userRole !== 'ADMIN'}
-            >
-              {departments.map((dept) => (
-                <option key={dept.id} value={dept.id}>
-                  {dept.name}
-                </option>
-              ))}
-            </Select>
-            <FormErrorMessage>{fieldState.error?.message}</FormErrorMessage>
-          </FormControl>
-        )}
-      />
-      <InputField
-        name="position"
-        label="Position"
-        isRequired
-        isDisabled={userRole !== 'ADMIN'}
-      />
+      <Tooltip
+        label="Department to which you belong."
+        bg={'#2D3748'}
+        fontSize="sm"
+        placement="top"
+        hasArrow
+      >
+        <Box>
+          <Controller
+            name="department"
+            render={({ field, fieldState }) => (
+              <FormControl
+                isRequired={userRole === 'ADMIN'}
+                isInvalid={!!fieldState.error}
+              >
+                <FormLabel>Department</FormLabel>
+                <Select
+                  {...field}
+                  placeholder="Select Department"
+                  borderColor="gray.400"
+                  borderWidth={1}
+                  isDisabled={userRole !== 'ADMIN'}
+                >
+                  {departments.map((dept) => (
+                    <option key={dept.id} value={dept.id}>
+                      {dept.name}
+                    </option>
+                  ))}
+                </Select>
+                <FormErrorMessage>{fieldState.error?.message}</FormErrorMessage>
+              </FormControl>
+            )}
+          />
+        </Box>
+      </Tooltip>
 
-      <Controller
-        name="role"
-        render={({ field }) => (
-          <FormControl isRequired isDisabled={userRole !== 'ADMIN'}>
-            <FormLabel>Role</FormLabel>
-            <Select {...field} borderColor={'gray.400'} borderWidth={1}>
-              <option value="CREW">CREW</option>
-              <option value="ADMIN">ADMIN</option>
-            </Select>
-          </FormControl>
-        )}
-      />
+      <Tooltip
+        label="Your position in the project."
+        bg={'#2D3748'}
+        fontSize="sm"
+        placement="top"
+        hasArrow
+      >
+        <Box>
+          <InputField
+            name="position"
+            label="Position"
+            isRequired
+            isDisabled={userRole !== 'ADMIN'}
+          />
+        </Box>
+      </Tooltip>
+
+      <Tooltip
+        label="Your role in the project (CREW or ADMIN)."
+        bg={'#2D3748'}
+        fontSize="sm"
+        placement="top"
+        hasArrow
+      >
+        <Box>
+          <Controller
+            name="role"
+            render={({ field }) => (
+              <FormControl isRequired isDisabled={userRole !== 'ADMIN'}>
+                <FormLabel>Role</FormLabel>
+                <Select {...field} borderColor={'gray.400'} borderWidth={1}>
+                  <option value="CREW">CREW</option>
+                  <option value="ADMIN">ADMIN</option>
+                </Select>
+              </FormControl>
+            )}
+          />
+        </Box>
+      </Tooltip>
     </SimpleGrid>
   </FormSection>
 );
--- frontend/src/shared/forms/CarForm.tsx
+++ frontend/src/shared/forms/CarForm.tsx
@@ -6,6 +6,7 @@ import {
   FormLabel,
   Input,
   SimpleGrid,
+  Tooltip,
 } from '@chakra-ui/react';
 import { FaCarSide } from 'react-icons/fa6';
 
@@ -36,42 +37,71 @@ export const CarForm: React.FC<CarFormProps> = ({
     pb={4}
     gridTemplateColumns={{ base: '1fr', md: '1.5fr 1fr 1fr auto' }}
   >
-    <FormControl>
-      <FormLabel>Vehicle Name</FormLabel>
-      <Input
-        value={carDetails.name}
-        placeholder="ex. personal, van, truck..."
-        onChange={(e) => setCarDetails({ ...carDetails, name: e.target.value })}
-      />
-    </FormControl>
-    <FormControl>
-      <FormLabel>Included kms</FormLabel>
-      <Input
-        value={carDetails.kilometer_allow}
-        placeholder="ex. 50"
-        type="number"
-        onChange={(e) =>
-          setCarDetails({
-            ...carDetails,
-            kilometer_allow: parseFloat(e.target.value),
-          })
-        }
-      />
-    </FormControl>
-    <FormControl>
-      <FormLabel>Extra km price</FormLabel>
-      <Input
-        value={carDetails.kilometer_rate}
-        placeholder="ex. 10"
-        type="number"
-        onChange={(e) =>
-          setCarDetails({
-            ...carDetails,
-            kilometer_rate: parseFloat(e.target.value),
-          })
-        }
-      />
-    </FormControl>
+    <Tooltip
+      label="Enter the name of the vehicle (e.g., personal, van, truck)"
+      bg={'#2D3748'}
+      fontSize="sm"
+      placement="top"
+      hasArrow
+    >
+      <FormControl>
+        <FormLabel>Vehicle Name</FormLabel>
+        <Input
+          value={carDetails.name}
+          placeholder="ex. personal, van, truck..."
+          onChange={(e) =>
+            setCarDetails({ ...carDetails, name: e.target.value })
+          }
+        />
+      </FormControl>
+    </Tooltip>
+
+    <Tooltip
+      label="Enter the number of kilometers included in the rate"
+      bg={'#2D3748'}
+      fontSize="sm"
+      placement="top"
+      hasArrow
+    >
+      <FormControl>
+        <FormLabel>Included kms</FormLabel>
+        <Input
+          value={carDetails.kilometer_allow}
+          placeholder="ex. 50"
+          type="number"
+          onChange={(e) =>
+            setCarDetails({
+              ...carDetails,
+              kilometer_allow: parseFloat(e.target.value),
+            })
+          }
+        />
+      </FormControl>
+    </Tooltip>
+
+    <Tooltip
+      label="Enter the price for extra kilometers beyond the allowance"
+      bg={'#2D3748'}
+      fontSize="sm"
+      placement="top"
+      hasArrow
+    >
+      <FormControl>
+        <FormLabel>Extra km price</FormLabel>
+        <Input
+          value={carDetails.kilometer_rate}
+          placeholder="ex. 10"
+          type="number"
+          onChange={(e) =>
+            setCarDetails({
+              ...carDetails,
+              kilometer_rate: parseFloat(e.target.value),
+            })
+          }
+        />
+      </FormControl>
+    </Tooltip>
+
     <Box
       display="flex"
       justifyContent={{ base: 'center', md: 'flex-end' }}
