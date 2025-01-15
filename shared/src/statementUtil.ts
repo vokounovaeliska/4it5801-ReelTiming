916975ecdfe665457f79987cc59ad146764d3f5b
Eliška Vokounová <vokounova.eliska@gmail.com>
2025-01-15 19:15:24 +0100

    optimalize timesheets calculations and useMemo

--- shared/src/statementUtil.ts
+++ shared/src/statementUtil.ts
@@ -18,70 +18,47 @@ export interface Timesheet {
 }
 
 export const statementUtil = {
-  calculateOvertimeAmount(statement: Timesheet): number {
-    let overtime = statement.claimed_overtime;
-    let totalOvertimeAmount = 0;
-
-    if (!statement.projectUser.rate) return totalOvertimeAmount;
-
-    if (overtime > 0) {
-      totalOvertimeAmount += statement.projectUser.rate?.overtime_hour1;
-      overtime -= 1;
-    }
-    if (overtime > 0) {
-      totalOvertimeAmount += statement.projectUser.rate?.overtime_hour2;
-      overtime -= 1;
-    }
-    if (overtime > 0) {
-      totalOvertimeAmount += statement.projectUser.rate.overtime_hour3;
-      overtime -= 1;
-    }
-    if (overtime > 0) {
-      totalOvertimeAmount +=
-        statement.projectUser.rate.overtime_hour4 * overtime;
-    }
-
-    return totalOvertimeAmount;
-  },
-
-  calculateKilometersOver(statement: Timesheet): number | null {
-    const allow = statement.car?.kilometer_allow ?? 0;
+  calculateStatementDetails(statement: Timesheet) {
+    const { rate } = statement.projectUser;
+    const claimedOvertime = statement.claimed_overtime;
+    const standardRate = rate?.standard_rate ?? 0;
+    const kilometerAllow = statement.car?.kilometer_allow ?? 0;
+    const kilometerRate = statement.car?.kilometer_rate ?? 0;
     const kilometers = statement.kilometers ?? 0;
+    let overtime = claimedOvertime;
+    let overtimeAmount = 0;
 
-    if (
-      statement.car?.kilometer_allow === null ||
-      statement.car?.kilometer_allow === undefined ||
-      !statement.kilometers
-    )
-      return null;
-
-    if (allow >= kilometers) {
-      return 0;
-    } else {
-      return kilometers - allow;
+    if (statement?.projectUser?.rate) {
+      if (overtime > 0) {
+        overtimeAmount += statement.projectUser.rate?.overtime_hour1;
+        overtime -= 1;
+      }
+      if (overtime > 0) {
+        overtimeAmount += statement.projectUser.rate?.overtime_hour2;
+        overtime -= 1;
+      }
+      if (overtime > 0) {
+        overtimeAmount += statement.projectUser.rate.overtime_hour3;
+        overtime -= 1;
+      }
+      if (overtime > 0) {
+        overtimeAmount += statement.projectUser.rate.overtime_hour4 * overtime;
+      }
     }
-  },
 
-  calculateKilometerSum(statement: Timesheet): number | null {
-    const over = statementUtil.calculateKilometersOver(statement);
-    const rate = statement.car?.kilometer_rate ?? 0;
+    const kilometersOver =
+      kilometers > kilometerAllow ? kilometers - kilometerAllow : 0;
+    const kilometerSum = kilometersOver * kilometerRate;
 
-    if (over == null || !rate) return null;
+    const totalCost = standardRate + overtimeAmount + kilometerSum;
 
-    if (over === 0) {
-      return 0;
-    } else {
-      return over * rate;
-    }
-  },
-
-  calculateTotalCost(statement: Timesheet): number | null {
-    const standardRate = statement.projectUser?.rate?.standard_rate ?? 0;
-    const kilometerAmount = statementUtil.calculateKilometerSum(statement) ?? 0;
-    return (
-      standardRate +
-      statementUtil.calculateOvertimeAmount(statement) +
-      kilometerAmount
-    );
+    return {
+      overtimeAmount,
+      kilometersOver,
+      kilometerSum,
+      totalCost,
+      standardRate,
+      claimedOvertime,
+    };
   },
 };
